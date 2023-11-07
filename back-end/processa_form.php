e76c7c26634960026c81847465e12059b4ba9c5a
Willian Castilho <willian.cp@edu.udesc.br>
2023-11-07 20:55:03 -0300

    Commit final

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -1,3 +1,15 @@
 <?php
-print_r($_GET);
+//print_r($_GET);
+$nome = $_GET('nome');
+$idade = $_GET('idade');
+
+echo '<h1> Dados do usuário</h1>';
+echo '<br>';
+echo '<p> O usuário $nome tem $idade anos.</p>';
+
+echo '<a href="http://localhost/udesc/index.html"><button>Home page</button></a>';
+echo '<button type="button" onclick="javascript:hisoty.back()">voltar</button>';
+echo 'button onclick="window.location.href=\'https:\\www.udesc.br\'"> Vai para UDESC</button>';
+
+
 ?>
